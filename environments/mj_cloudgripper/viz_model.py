d1717508304938099f86615443ad921f838a41e6
lastsean <sean.maroofi@berkeley.edu>
2026-07-18 14:11:38 +0200

    added cloudgripper blender

---
+++ environments/mj_cloudgripper/viz_model.py
@@ -0,0 +1,13 @@
+import mujoco
+
+xml = """
+<mujoco>
+  <worldbody>
+    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+  </worldbody>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_path("environments/mj_cloudgripper/cloudgripper_cell.xml")
+
+print(model.names)
